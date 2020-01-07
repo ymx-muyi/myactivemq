259b22169ea54917891e2be0ba43ed727ee859e3
ymx <1668710767@qq.com>
2020-01-07 08:15:43 +0800

    springboot

---
+++ src/main/java/com/springboot/activemq/springboot_activemq/SpringbootActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.activemq.springboot_activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActivemqApplication.class, args);
+    }
+
+}
